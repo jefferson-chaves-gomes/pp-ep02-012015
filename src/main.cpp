d12224f9f59a87e27ddf6810c9e2a1fdcd14bbc7
Jefferson Chaves Gomes <jefferson.gomes@brisa.org.br>
2015-04-17 19:59:02 -0300

    Partial implementation

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,9 @@
 // Libraries definitions
 // -----------------------------------------------------
 #include <iostream>
-#include <time.h>
-#include <cmath>
+#include <time.h> //
+#include <cmath> // for std::abs use
+#include <iomanip> // for std::setprecision use
 
 // Enums definitions
 // -----------------------------------------------------
@@ -27,43 +28,43 @@ enum OutputType {
 // -----------------------------------------------------
 struct Result {
         std::chrono::duration<double> processTime;
-        double area;
+        long double area;
 };
 
 // Functions declarations
 // -----------------------------------------------------
-bool readInputParams(int, char**, OutputType&, long&);
+bool readInputParams(int, char**, OutputType&, int&);
 void printUsage();
 OutputType stringToOutputType(std::string);
-void readVertices(long, double[][2]);
-Result shoelaceTheorem(long, double[][2], long);
+void readVertices(long, float[][2]);
+Result shoelaceTheorem(long, const float[][2], long);
 void printResult(OutputType, Result);
 
 // main Function
 // -----------------------------------------------------
 int main(int argc, char **argv) {
-    long numVertices = 0;
-    long numThreads = 0;
+    int numThreads = 0;
     OutputType outputType = NONE;
     if (readInputParams(argc, argv, outputType, numThreads)) {
+        long numVertices = 0;
         std::cin >> numVertices;
-        double arrayVertices[numVertices][2];
+        float arrayVertices[numVertices][2];
         readVertices(numVertices, arrayVertices);
-        shoelaceTheorem(numVertices, arrayVertices, numThreads);
+        Result result = shoelaceTheorem(numVertices, arrayVertices, numThreads);
+        printResult(outputType, result);
     }
-    std::cout << "\n\nFinalizando:\n";
     return EXIT_SUCCESS;
 }
 
 // Function implementations
 // -----------------------------------------------------
-bool readInputParams(int argc, char **argv, OutputType &outputType, long &numThreads) {
+bool readInputParams(int argc, char **argv, OutputType &outputType, int &numThreads) {
     if (argc != 3) {
         printUsage();
         return false;
     } else {
         std::string input(argv[1]);
-        numThreads = std::atol(argv[2]);
+        numThreads = std::atoi(argv[2]);
         OutputType outputTypel = stringToOutputType(input);
         if (numThreads <= 0 || numThreads > INT_MAX || outputTypel == NONE) {
             printUsage();
@@ -94,37 +95,51 @@ OutputType stringToOutputType(std::string input) {
     return NONE;
 }
 
-void readVertices(long numVertices, double arrayVertices[][2]) {
-    for (long i = 0; i < numVertices; ++i) {
+void readVertices(const long numVertices, float arrayVertices[][2]) {
+    for (long i = 0; i < numVertices; i++) {
+        tratar apenas
+        os vértices
         std::cin >> arrayVertices[i][0];
         std::cin >> arrayVertices[i][1];
     }
 }
 
-Result shoelaceTheorem(long numVertices, double arrayVertices[][2], long numThreads) {
+Result shoelaceTheorem(long numVertices, const float arrayVertices[][2], long numThreads) {
     Result result;
     long double sum1 = 0;
     long double sum2 = 0;
     double x = 0;
     double y = 0;
-    for (int i = 0; i < numVertices - 1; i++) {
+    for (long i = 0; i < numVertices - 1; i++) {
         x = arrayVertices[i][0];
         y = arrayVertices[i + 1][1];
         sum1 += x * y;
-    }
-    sum1 += arrayVertices[4][0] * arrayVertices[1][1];
-    for (int i = 0; i < numVertices - 1; i++) {
+//    sum1 += arrayVertices[4][0] * arrayVertices[1][1];
         x = arrayVertices[i + 1][0];
         y = arrayVertices[i][1];
+//    sum2 -= arrayVertices[1][0] * arrayVertices[4][1];
         sum2 += x * y;
     }
-    sum2 += arrayVertices[1][0] * arrayVertices[4][1];
-
-    long double area = std::abs((sum1 - sum2) / 2.0);
-    std::cout << " area: " << area << "\n";
+    result.area = std::abs((sum1 - sum2) / 2.0);
     return result;
 }
 
 void printResult(OutputType outputType, Result result) {
-
+    std::cout << std::endl;
+    switch (outputType) {
+        case TIME:
+            std::cout << result.processTime.count() << "time \n";
+            break;
+        case AREA:
+            std::cout << std::fixed;
+            std::cout << std::setprecision(2) << result.area << "area \n";
+            break;
+        case ALL:
+            std::cout << std::fixed;
+            std::cout << std::setprecision(2) << result.area << "area \n";
+            std::cout << result.processTime.count() << "time \n";
+            break;
+        default:
+            break;
+    }
 }
