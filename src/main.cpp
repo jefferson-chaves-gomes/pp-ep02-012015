25e3449dd0ddc17beedbb491ff9e377700e253d0
Jefferson Chaves Gomes <jefferson.chaves@gmail.com>
2015-04-22 01:35:18 -0300

    final implementation

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,14 @@
 
 // Libraries definitions
 // -----------------------------------------------------
-#include <iostream>
-#include <time.h> //
-#include <cmath> // for std::abs use
-#include <iomanip> // for std::setprecision use
+#include <iostream> // for std::cin, std::cout, etc.
+#include <vector> // for vector
+#include <sys/time.h> // for gettimeofday
+#include <cmath> // for std::abs, std::ceil
+#include <iomanip> // for std::setprecision
+#include <pthread.h> // for pthreads
+#include <stdlib.h> // for std:atoi
+#include <climits> // for INT_MAX
 
 // Enums definitions
 // -----------------------------------------------------
@@ -27,8 +31,57 @@ enum OutputType {
 // Structs definitions
 // -----------------------------------------------------
 struct Result {
-        std::chrono::duration<double> processTime;
+        long processTime;
         long double area;
+};
+
+// Classes definitions
+// -----------------------------------------------------
+class Vertice {
+    private:
+        double x;
+        double y;
+    public:
+        Vertice(double arg0, double arg1) :
+                x(arg0), y(arg1) {
+            // Nothing to do.
+        }
+        double getX() const {
+            return x;
+        }
+        double getY() const {
+            return y;
+        }
+};
+
+class RlThreadRangeVertices {
+    private:
+        long pThreadId;
+        long beginIndex;
+        long endIndex;
+        Result result;
+    public:
+        RlThreadRangeVertices(long arg0, long arg1, long arg2, Result arg3) :
+                pThreadId(arg0), beginIndex(arg1), endIndex(arg2), result(arg3) {
+            // Nothing to do.
+        }
+
+        long getThreadId() const {
+            return pThreadId;
+        }
+        long getBeginIndex() const {
+            return beginIndex;
+        }
+        long getEndIndex() const {
+            return endIndex;
+        }
+        const Result& getResult() const {
+            return result;
+        }
+        void setArea(const long double area) {
+            this->result.area = area;
+        }
+
 };
 
 // Functions declarations
@@ -36,9 +89,15 @@ struct Result {
 bool readInputParams(int, char**, OutputType&, int&);
 void printUsage();
 OutputType stringToOutputType(std::string);
-void readVertices(long, float[][2]);
-Result shoelaceTheorem(long, const float[][2], long);
+void readVertices(long, std::vector<Vertice>&);
+void buildRelations(long, int, std::vector<RlThreadRangeVertices>&);
+Result startParallelProcess(const std::vector<RlThreadRangeVertices>&);
+void *shoelaceTheorem(void*);
 void printResult(OutputType, Result);
+
+// Globals declarations
+// -----------------------------------------------------
+std::vector<Vertice> vecVertices;
 
 // main Function
 // -----------------------------------------------------
@@ -48,9 +107,10 @@ int main(int argc, char **argv) {
     if (readInputParams(argc, argv, outputType, numThreads)) {
         long numVertices = 0;
         std::cin >> numVertices;
-        float arrayVertices[numVertices][2];
-        readVertices(numVertices, arrayVertices);
-        Result result = shoelaceTheorem(numVertices, arrayVertices, numThreads);
+        readVertices(numVertices, vecVertices);
+        std::vector<RlThreadRangeVertices> vecRelations;
+        buildRelations(numVertices, numThreads, vecRelations);
+        Result result = startParallelProcess(vecRelations);
         printResult(outputType, result);
     }
     return EXIT_SUCCESS;
@@ -95,49 +155,82 @@ OutputType stringToOutputType(std::string input) {
     return NONE;
 }
 
-void readVertices(const long numVertices, float arrayVertices[][2]) {
+void readVertices(const long numVertices, std::vector<Vertice> &vecVertices) {
+    double x = 0.0;
+    double y = 0.0;
     for (long i = 0; i < numVertices; i++) {
-        tratar apenas
-        os vértices
-        std::cin >> arrayVertices[i][0];
-        std::cin >> arrayVertices[i][1];
+        std::cin >> x;
+        std::cin >> y;
+        vecVertices.push_back(Vertice(x, y));
     }
 }
 
-Result shoelaceTheorem(long numVertices, const float arrayVertices[][2], long numThreads) {
-    Result result;
-    long double sum1 = 0;
-    long double sum2 = 0;
-    double x = 0;
-    double y = 0;
-    for (long i = 0; i < numVertices - 1; i++) {
-        x = arrayVertices[i][0];
-        y = arrayVertices[i + 1][1];
-        sum1 += x * y;
-//    sum1 += arrayVertices[4][0] * arrayVertices[1][1];
-        x = arrayVertices[i + 1][0];
-        y = arrayVertices[i][1];
-//    sum2 -= arrayVertices[1][0] * arrayVertices[4][1];
-        sum2 += x * y;
+void buildRelations(long numVertices, int numThreads, std::vector<RlThreadRangeVertices>& vecRelations) {
+    long chunkSize = std::ceil((double) (numVertices) / numThreads);
+    long beginIndex = 0;
+    long endIndex = chunkSize - 1;
+    for (long i = 0; i < numThreads; i++) {
+        Result result;
+        vecRelations.push_back(RlThreadRangeVertices(i, beginIndex, endIndex, result));
+        if (endIndex + 1 == numVertices) {
+            break;
+        }
+        beginIndex = endIndex + 1;
+        endIndex = beginIndex + chunkSize - 1;
+        if (numVertices <= endIndex) {
+            endIndex = numVertices - 1;
+        }
     }
-    result.area = std::abs((sum1 - sum2) / 2.0);
+}
+
+Result startParallelProcess(const std::vector<RlThreadRangeVertices>& vecRelations) {
+    Result result;
+    timeval startTime;
+    timeval endTime;
+    gettimeofday(&startTime, NULL);
+    pthread_t* arrayThreads = new pthread_t[vecRelations.size()];
+    for (int i = 0; i < vecRelations.size(); i++) {
+        pthread_create(&arrayThreads[i], NULL, shoelaceTheorem, (void *) &vecRelations[i]);
+    }
+    long double sum = 0.0;
+    for (int i = 0; i < vecRelations.size(); i++) {
+        pthread_join(arrayThreads[i], NULL);
+        sum += vecRelations[i].getResult().area;
+    }
+    result.area = std::abs((sum) / 2.0);
+    gettimeofday(&endTime, NULL);
+    long time = ((endTime.tv_sec * 1000000 + endTime.tv_usec) - (startTime.tv_sec * 1000000 + startTime.tv_usec));
+    result.processTime = time;
     return result;
+}
+
+void *shoelaceTheorem(void *arg0) {
+    // Cast the received parameter.
+    RlThreadRangeVertices *pThreadArgs = (RlThreadRangeVertices *) arg0;
+    long double sum1 = 0.0;
+    long double sum2 = 0.0;
+    for (long i = pThreadArgs->getBeginIndex(); i <= pThreadArgs->getEndIndex(); i++) {
+        sum1 += vecVertices[i].getX() * vecVertices[i + 1].getY();
+        sum2 += vecVertices[i + 1].getX() * vecVertices[i].getY();
+    }
+    pThreadArgs->setArea(sum1 - sum2);
+    pthread_exit(arg0);
 }
 
 void printResult(OutputType outputType, Result result) {
     std::cout << std::endl;
     switch (outputType) {
         case TIME:
-            std::cout << result.processTime.count() << "time \n";
+            std::cout << result.processTime << "\n";
             break;
         case AREA:
             std::cout << std::fixed;
-            std::cout << std::setprecision(2) << result.area << "area \n";
+            std::cout << std::setprecision(2) << result.area << "\n";
             break;
         case ALL:
             std::cout << std::fixed;
-            std::cout << std::setprecision(2) << result.area << "area \n";
-            std::cout << result.processTime.count() << "time \n";
+            std::cout << std::setprecision(2) << result.area << "\n";
+            std::cout << result.processTime << "\n";
             break;
         default:
             break;
